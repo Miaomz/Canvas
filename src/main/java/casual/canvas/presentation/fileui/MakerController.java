813031fc93560dd95436115ccdd966c81bc2a799
Miaomz <592589198@qq.com>
2018-09-21 10:59:35 +0800

    complete mode selecting function

---
+++ src/main/java/casual/canvas/presentation/fileui/MakerController.java
@@ -0,0 +1,66 @@
+package casual.canvas.presentation.fileui;
+
+import casual.canvas.util.LoggerUtil;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+import java.util.List;
+
+import static casual.canvas.util.ConstantString.EXTENSION;
+
+/**
+ * @author miaomuzhi
+ * @since 2018/9/17
+ */
+public class MakerController {
+
+    @FXML
+    private TextField inputField;
+
+    private List<String> strings;//space to store the file
+
+
+    public static void initFileMaker(List<String> memorySpace){
+        try {
+            FXMLLoader loader = new FXMLLoader(MakerController.class.getResource("/fxml/filemaker.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.initStyle(StageStyle.UTILITY);
+            stage.setScene(scene);
+
+            loader.<MakerController>getController().strings = memorySpace;
+
+            stage.showAndWait();
+        } catch (IOException e){
+            LoggerUtil.getLogger().info(e);
+        }
+    }
+
+    @FXML
+    private void cancel(){
+        ((Stage)inputField.getScene().getWindow()).close();
+    }
+    @FXML
+    private void confirm(){
+        String input = inputField.getText();
+        if (input == null || input.isEmpty()){
+            return;
+        }
+
+        if (!input.endsWith(EXTENSION)){
+            input = input + EXTENSION;
+        }
+        strings.add(input);
+        ((Stage)inputField.getScene().getWindow()).close();
+    }
+}
